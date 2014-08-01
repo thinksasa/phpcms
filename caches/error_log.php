98f69d23a3d00986d24962104652bceded5e24f2
lee <thinksasa@163.com>
2014-08-01 18:45:58 +0800

    add .gitignore file

--- caches/error_log.php
+++ caches/error_log.php
@@ -28,3 +28,15 @@
 <?php exit;?>07-31 18:39:47 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
 <?php exit;?>07-31 18:39:47 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
 <?php exit;?>07-31 18:39:47 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>08-01 18:14:03 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>08-01 18:15:49 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>08-01 18:15:49 | 2 | imagecreatefromjpeg() [<a href='function.imagecreatefromjpeg'>function.imagecreatefromjpeg</a>]: gd-jpeg: JPEG library reports unrecoverable error:  | phpcms\libs\classes\image.class.php | 102
+<?php exit;?>08-01 18:15:55 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>08-01 18:15:55 | 2 | imagecreatefromjpeg() [<a href='function.imagecreatefromjpeg'>function.imagecreatefromjpeg</a>]: gd-jpeg: JPEG library reports unrecoverable error:  | phpcms\libs\classes\image.class.php | 102
+<?php exit;?>08-01 18:16:36 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>08-01 18:16:36 | 2 | imagecreatefromjpeg() [<a href='function.imagecreatefromjpeg'>function.imagecreatefromjpeg</a>]: gd-jpeg: JPEG library reports unrecoverable error:  | phpcms\libs\classes\image.class.php | 102
+<?php exit;?>08-01 18:17:31 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
+<?php exit;?>08-01 18:17:31 | 2 | imagecreatefromjpeg() [<a href='function.imagecreatefromjpeg'>function.imagecreatefromjpeg</a>]: gd-jpeg: JPEG library reports unrecoverable error:  | phpcms\libs\classes\image.class.php | 102
+<?php exit;?>08-01 18:18:34 | 2 | imagecreatefromjpeg() [<a href='function.imagecreatefromjpeg'>function.imagecreatefromjpeg</a>]: gd-jpeg: JPEG library reports unrecoverable error:  | phpcms\libs\classes\image.class.php | 102
+<?php exit;?>08-01 18:18:36 | 2 | imagecreatefromjpeg() [<a href='function.imagecreatefromjpeg'>function.imagecreatefromjpeg</a>]: gd-jpeg: JPEG library reports unrecoverable error:  | phpcms\libs\classes\image.class.php | 102
+<?php exit;?>08-01 18:18:41 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
